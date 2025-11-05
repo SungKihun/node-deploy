48c8660cc1bd9b5e1f0ef690ceea0a2dd82e621b
SungKihun <tjdrlgns3293@gmail.com>
2025-11-05 21:03:25 +0900

    require('connect-redis') 주석 제거

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const redis = require('redis');
-// const RedisStore = require('connect-redis')(session);
-// const RedisStore = require('connect-redis').default;
 const { RedisStore } = require('connect-redis');
 
 dotenv.config();
